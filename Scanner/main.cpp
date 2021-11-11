63467c277993656c0acde82c9e8b8e31a82d5d1e
haost74 <haost@yandex.ru>
2021-11-11 11:24:49 +0300

    non-blocking socket

--- Scanner/main.cpp
+++ Scanner/main.cpp
@@ -5,11 +5,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
 #include <iostream>
 #include <string>
 
-
+void non_blocking_socket(char* addr, int port);
 int main()
 {
 
@@ -28,4 +29,49 @@ int isConn = connect(sockfd, res->ai_addr, res->ai_addrlen);
     std::cout << isConn << '\n';
    
     return 0;
+}
+
+void non_blocking_socket(char* addr, int port)
+{
+	int sockfd;
+	struct addrinfo hints, *res;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(addr, std::to_string(port).c_str(), &hints, &res);
+
+	// make a socket:
+
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	//fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+	 fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+	// connect!
+
+	auto idCon = connect(sockfd, res->ai_addr, res->ai_addrlen);
+
+
+	 FD_ZERO(&fdset);
+	 FD_SET(sockfd, &fdset);
+		tv.tv_sec = 7;             /* 10 second timeout */
+		tv.tv_usec = 0;
+
+		if (select(sockfd + 1, NULL, &fdset, NULL, &tv) == 1)
+		{
+			int so_error;
+			socklen_t len = sizeof so_error;
+			 getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &so_error, &len);
+			 if (so_error == 0) {
+				printf("%s:%d is open\n", addr, port);
+			}
+		}
+
+	std::cout << idCon << '\n';
+
+	close(sockfd);
+	close(idCon);
+	freeaddrinfo(res); // free the linked list
 }
